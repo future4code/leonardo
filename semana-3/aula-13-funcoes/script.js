6c690dd9f739c015da9783b1334281ddfa8697c2
Leonardo <leonardocpn@gmail.com>
2019-09-23 16:42:50 -0300

    entrega final

--- semana-3/aula-13-funcoes/script.js
+++ semana-3/aula-13-funcoes/script.js
@@ -1,39 +1,19 @@
 function criarItem(){
     let itemInput = document.querySelector("input").value;
-    let diaSemana = document.getElementById("diaDaSemana").value;
-    if (itemInput === ""){
-        alert("Informe a tarefa que deseja adicionar");
-    }else if (diaSemana === "segunda"){
-        document.getElementById("segunda").innerHTML += "<li>" + itemInput + "</li>";
-        document.getElementById("segunda").onclick = riscaItem;
-    }else if (diaSemana === "terca"){
-        document.getElementById("terca").innerHTML += "<li>" + itemInput + "</li>";
-        document.getElementById("terca").onclick = riscaItem;
-    }else if (diaSemana === "quarta"){
-        document.getElementById("quarta").innerHTML += "<li>" + itemInput + "</li>";
-        document.getElementById("terca").onclick = riscaItem;
-    }else if (diaSemana === "quinta"){
-        document.getElementById("quinta").innerHTML += "<li>" + itemInput + "</li>";
-        document.getElementById("terca").onclick = riscaItem;
-    }else if (diaSemana === "sexta"){
-        document.getElementById("sexta").innerHTML += "<li>" + itemInput + "</li>";
-        document.getElementById("terca").onclick = riscaItem;
-    }else if (diaSemana === "sabado"){
-        document.getElementById("sabado").innerHTML += "<li>" + itemInput + "</li>";
-        document.getElementById("terca").onclick = riscaItem;
-    }else if (diaSemana === "domingo"){
-        document.getElementById("domingo").innerHTML += "<li>" + itemInput + "</li>";
-        document.getElementById("terca").onclick = riscaItem;
-    }
+    let diaSemana = document.querySelector("#" + diaDaSemana.value);
+    let newLi = document.createElement("li");
+    newLi.innerHTML = itemInput;
+    newLi.onclick = riscaItem;
+    diaSemana.appendChild(newLi);
     document.querySelector("input").value = "";
 }
 
 function riscaItem(event){
     const target = event.target;
     const value = target.innerHTML;
-    value.innerHTML = "teste";
+    target.parentNode.removeChild(target);
 }
 
-function Limpar(){
-
+function limparTela(){
+    window.location.reload();
 }
