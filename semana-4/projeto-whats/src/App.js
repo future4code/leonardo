34c9d94692add905e5a87bf6e0ee5c6bc15f46f4
Leonardo <leonardocpn@gmail.com>
2019-10-04 14:34:59 -0300

    Projeto para git

--- semana-4/projeto-whats/src/App.js
+++ semana-4/projeto-whats/src/App.js
@@ -1,9 +1,53 @@
 import React from 'react';
-// import Inserir from './Inserir/Inserir'
-// import Exibir from './Exibir/Exibir'
 import './App.css';
 import {Linha} from './Linha/Linha'
+import styled from 'styled-components'
 
+const Container = styled.div `
+  display:flex;
+  justify-content:center;
+  box-sizing: border-box;
+  border:0;
+  margin:0;
+  padding:0;
+`
+
+const Tela = styled.div `
+max-width: 600px;
+height: 100vh;
+display: flex;
+flex-direction: column;
+border-width: 1px;
+border-style: solid;
+border-color: black;
+border-image: initial;
+flex: 1 1 0%;
+`
+const EntradaMensagem = styled.section `
+  flex-grow:0;
+  margin-bottom: 20px;
+`
+const CaixaMensagem = styled.section `
+display: flex;
+flex-direction: column;
+justify-content: flex-end;
+flex: 1 1 0%;
+padding: 20px;
+   
+`
+
+const Usuario = styled.input `
+width:20%;
+`
+const TextoDigitado = styled.input `
+width: 60%;
+`
+const ButtonEnviar = styled.button `
+  margin:0;
+  border:0;
+  padding:0;
+  width:20%;
+`
 class App extends React.Component {
   constructor(props){
       super(props);
@@ -50,16 +94,22 @@ class App extends React.Component {
       return <Linha
               key={index}
               nomeUsuario={linha.nomeUsuario}
-              texto={linha.mensagem}
+              texto={": " + linha.mensagem}
               />
     })
     return (
-    <section>
-      {exibirMensagens}
-      <input type="text" placeholder="usuario" value={this.state.nomeUsuario} onChange={this.onChangeUsuario}/>
-      <input type="text" placeholder="Mensagem" value={this.state.mensagem} onChange={this.onChangeMensagem}/>
-      <button onClick={this.inserirMensagem}>Enviar</button>
-    </section>
+    <Container>
+      <Tela>
+        <CaixaMensagem>
+          {exibirMensagens}
+        </CaixaMensagem>
+        <EntradaMensagem>
+          <Usuario type="text" placeholder="usuario" value={this.state.nomeUsuario} onChange={this.onChangeUsuario}/>
+          <TextoDigitado type="text" placeholder="Mensagem" value={this.state.mensagem} onChange={this.onChangeMensagem}/>
+          <ButtonEnviar onClick={this.inserirMensagem}>Enviar</ButtonEnviar>
+        </EntradaMensagem>
+      </Tela>
+    </Container>
   );
 }
 }
--- semana-4/projeto-whats/src/Linha/Linha.js
+++ semana-4/projeto-whats/src/Linha/Linha.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 export {Linha}
+
+const Negrito = styled.span `
+    font-weight: bold;
+`
+
+const LinhaDeTexto = styled.p `
+  display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+`
 
 class Linha extends React.Component {
   constructor(props){
@@ -11,9 +24,9 @@ class Linha extends React.Component {
 
   render(){
     return (
-    <article>
-      <p><span>{this.props.nomeUsuario}</span> {this.props.texto}</p>
-    </article>
+    
+      <LinhaDeTexto><Negrito>{this.props.nomeUsuario}    </Negrito> {this.props.texto}</LinhaDeTexto>
+    
   );
 }
 }
