55efbd9e23ee3d9ef243f899f3e6bfee70e857fc
Leonardo <leonardocpn@gmail.com>
2019-10-17 15:00:31 -0300

    entrega parcial

---
+++ semana-6/aula-27-browser-servidor/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BoredApiTest from  './Components/BoredApiTest/BoredApiTest'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BoredApiTest/>
+      
+    </div>
+  );
+}
+
+export default App;
