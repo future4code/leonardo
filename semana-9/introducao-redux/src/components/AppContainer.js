03ef9cc61b8e9dfd3494de2ee7e750d5b96811e6
Leonardo <leonardocpn@gmail.com>
2019-10-29 13:11:49 -0300

    novo comeco

--- semana-9/introducao-redux/src/components/AppContainer.js
+++ semana-9/introducao-redux/src/components/AppContainer.js
@@ -1,40 +1,4 @@
 import React from 'react'
-import { createStore } from "redux";
-import { useReduxState, Provider, useDispatch } from "../redux/react-redux-f4";
-
-const initialState = {
-	lista: [
-		todo = {
-			id: '',
-			nome: '',
-			concluido: '',
-		}]
-};
-
-
-function reducer(state = initialState, action) {
-	switch (action.type) {
-		case "ADD_TODO":
-			return {...state	}
-		case "DEL_TODO":
-			return { ...state, counter: state.counter - 1 };
-		case "EDIT":
-			return { lista: [] };
-		case "RESET":
-			return { lista: [] };
-		case "COMPLETE_TODO":
-			return { lista:[]};
-		case "COMPLETE_ALL_TODOS":
-			return { lista:[]};
-		case "FILTER":
-			return { lista: [] };
-		default:
-			return state;
-	}
-}
-
-const store = createStore(reducer);
-
 
 export class AppContainer extends React.Component {
 	constructor(props) {
