2eebb071fd074b16cfb9d3cbee7a56c0767f8523
Leonardo <leonardocpn@gmail.com>
2019-11-01 15:13:03 -0300

    entrega parcial com build

---
+++ semana-9/astro-match/build/precache-manifest.bbd65041d7ea26caac3b1a174391c798.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74730978694f055f763a2b1c100e2025",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e3e096c07ad6ee55eee",
+    "url": "/static/js/2.13d7d239.chunk.js"
+  },
+  {
+    "revision": "f3a02d93b950d72b9a97",
+    "url": "/static/js/main.a8b2355d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
