70f1e6b95f00206024ebdb29e5a57665625409b4
Leonardo <leonardocpn@gmail.com>
2019-12-03 15:23:01 -0300

    entrega parcial

---
+++ semana-12/aula-53/src/ex1.ts
@@ -0,0 +1,30 @@
+import { totalmem } from "os";
+
+const arr = [1,2,3,4,5]
+type obj = {
+    lengthArr: number;
+    numImpar: number;
+    soma: number;
+  };
+
+function array( arr: number[]): obj {
+    let lengthArr = arr.length
+    let numImpar: number[] = arr.filter((el: number) => {
+        return el%2 !==0
+    })
+    let impar: number = numImpar.length
+    let soma: number = 0;
+    for(let i = 0; i < arr.length; i++){
+        soma+= arr[i];
+    }
+
+    const obj: obj = {
+        lengthArr,
+        numImpar:impar,
+        soma
+    }
+    return  obj
+}
+
+console.log(array(arr))
+
